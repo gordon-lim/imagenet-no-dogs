c18da97e6d24a88169ecaaca7bccebf8cccfadfb
gordon-lim <gordonlim214@gmail.com>
2024-02-03 15:01:08 -0500

    Removed pretrained

--- hubconf.py
+++ hubconf.py
@@ -4,18 +4,17 @@ import torch
 from torchvision.models.resnet import resnet50 as _resnet50
 
 # resnet18 is the name of entrypoint
-def resnet50(pretrained=False, **kwargs):
+def resnet50(**kwargs):
     """ # This docstring shows up in hub.help()
     Resnet50 model
     pretrained (bool): kwargs, load pretrained weights into the model
     """
     # Call the model, load pretrained weights
-    model = _resnet50(pretrained=pretrained, **kwargs)
+    model = _resnet50(**kwargs)
     num_ftrs = model.fc.in_features
     model.fc = torch.nn.Linear(num_ftrs, 880)
 
-    if pretrained:
-        checkpoint = 'https://cumberland.isis.vanderbilt.edu/gordon/model_best.pth.tar'
-        model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint, progress=False))
+    checkpoint = 'https://cumberland.isis.vanderbilt.edu/gordon/model_best.pth.tar'
+    model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint, progress=False))
 
     return model
